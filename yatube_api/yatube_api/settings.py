a0d29cc04094c526101dc13dcc3a8cb031554d06
Kirill Sirotkin <kirasir2006@gmail.com>
2026-08-28 22:55:35 +0300

    DRF Settings

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -106,3 +106,12 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
