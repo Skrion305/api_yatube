8cd1972b69033c7d4ee3bb8b94fb730b788758ee
Kirill Sirotkin <kirasir2006@gmail.com>
2026-08-29 18:17:20 +0300

    Corrections

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from posts.models import Post, Group, Comment
 
+
 class PostSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
         read_only=True, slug_field='username'
